6d15ba4f93648d2ea883827f763f1e185e427f44
0Aleksader <aleks.laketa@gmail.com>
2026-01-13 10:24:52 +0100

    text

---
+++ Vaja/Vaja/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Na kmetiji je lepo iaiaia tam ima dva oslka iaiaia o");
+        }
+    }
+}
